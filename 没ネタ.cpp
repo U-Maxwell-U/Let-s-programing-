99e59fe7c860401e0ce09490daa3129468116497
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 22:20:02 +0900

    Create 没ネタ.cpp

---
+++ 没ネタ.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    int x;
+    std::cin >> x[6] = {3, 5, 11, 7, 8, 19, 0};
+    switch (x = 0){
+        case 1:
+            std::cout << "Case 1:" << x[0] << std::endl;
+        case 2:
+            std::cout << "Case 2:" << x[1] << std::endl;
+        case 3:
+            std::cout << "Case 3:" << x[2] << std::endl;
+        case 4:
+            std::cout << "Case 4:" << x[3] << std::endl;
+        case 5:
+            std::cout << "Case 5:" << x[4] << std::endl;
+        case 6:
+            std::cout << "Case 6:" << x[5] << std::endl;
+            break;
+        default:
+    }
+    
+
+	return 0;
+}

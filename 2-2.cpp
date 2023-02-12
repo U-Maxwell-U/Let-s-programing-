8d5e3ec30a26a73814841b9df3cc7d39a54fe78e
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 14:31:00 +0900

    Update 2-2.cpp

--- 2-2.cpp
+++ 2-2.cpp
@@ -3,7 +3,7 @@
 int main(){
     int a, b, c;
     std::cin >> a >> b >> c;
-    if (a < b < c){
+    if (a < b && b << c){ //アイズの仕様上 a < b < c の書き方はアクセプトしてくれない
         std::cout << "Yes" << std::endl;
     }
 

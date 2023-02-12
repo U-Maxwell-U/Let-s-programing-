824d7ac0dca770fea601d12543e778f9ba1b06a6
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 10:54:00 +0900

    Create 2-2.cpp

---
+++ 2-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    int a, b, c;
+    std::cin >> a >> b >> c;
+    if (a < b < c){
+        std::cout << "Yes" << std::endl;
+    }
+
+    else {
+        std::cout << "No" << std::endl;
+    }
+
+    return 0;
+}

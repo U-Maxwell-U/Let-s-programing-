1e4643f77209527835fe0fe0fa62e414778e0259
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 10:43:48 +0900

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main(){
+    int a, b;
+    std::cin >> a >> b;
+    if (a < b){
+        std::cout << "a < b" << std::endl;
+    }
+
+    else if (a > b){
+        std::cout << "a > b" << std::endl;
+    }
+
+    else if (a == b){
+        std::cout << "a == b" << std::endl;
+    }
+
+    return 0;
+}

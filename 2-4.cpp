9b48a729d54286fc5959b95c99a5bad5ea9b355f
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 20:10:59 +0900

    Create 2-4.cpp

---
+++ 2-4.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+int a, b, c,;
+
+int main(){
+
+    cin >> a >> b >> c;
+    int syouzyun[3] = {a, b, c};
+    sort(syouzyun, syouzyun + 3);
+    cout << syouzyun[0] << " " << syouzyun[1] << " " << syouzyun[2] << endl;
+
+    return 0;
+}

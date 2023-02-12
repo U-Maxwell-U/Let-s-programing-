c9213d06e08b95a6f4fbc492548ffff413e325dd
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-12 21:27:10 +0900

    Create 2-5.cpp

---
+++ 2-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	int w, h, x, y, r;
+	
+    std::cin >> w >> h >> x >> y >> r;
+	if (r <= x && r <= y && x <= (w - r) && y <= (h - r)) {
+        std::cout << "Yes" << std::endl;
+    }
+	else {
+        std::cout << "No" << std::endl;
+    }
+	return 0;
+}

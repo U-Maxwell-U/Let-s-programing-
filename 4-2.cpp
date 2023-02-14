16de4f154c84caa2393ca916839af73002ded777
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-14 23:30:03 +0900

    Update 4-2.cpp

--- 4-2.cpp
+++ 4-2.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <bits/stdc++.h>
 
+int main() {
+	double r;
+	double c, m;
+
+	std::cin >> r;
+
+	m = r * r * M_PI;
+    c = 2 * M_PI * r;
+
+	std::cout << std::fixed << m << " " << std::fixed << c << std::endl;
+
+	return 0;
+}

e45c105c6e3bb1a9e50d0715654a05681ae91f51
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-14 13:52:27 +0900

    Update 3-3.cpp

--- 3-3.cpp
+++ 3-3.cpp
@@ -1,3 +1,25 @@
+// #include <iostream>
+
+
+// int main(){
+
+//     int x, y;
+//     std::cin >> x >> y;
+//     while((x != 0 && y != 0) || (x != 0 && y == 0) || (x == 0 && y != 0)){
+
+//         std::swap(x, y);
+//         std::cout<< x << " " << y << std::endl;
+//         std::cin >> x >> y;
+//     }
+    
+//     return 0;
+// }
+
+
+
+
+
+
 #include <iostream>
 
 
@@ -5,9 +27,11 @@ int main(){
 
     int x, y;
     std::cin >> x >> y;
-    while((x != 0 && y != 0) || (x != 0 && y == 0) || (x == 0 && y != 0)){
+    while(x || y){
 
-        std::swap(x, y);
+        if (y < x){
+            std::swap(y, x);
+        }
         std::cout<< x << " " << y << std::endl;
         std::cin >> x >> y;
     }

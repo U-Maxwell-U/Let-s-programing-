55fe70024ec18322269b75d45c4ca23482a6b6e9
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-16 09:41:41 +0900

    Update 4-4.cpp

--- 4-4.cpp
+++ 4-4.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include <climits>
+#include <cmath>
+
+int main(){
+	int n;
+	std::cin >> n;
+	
+	long min, max;
+	long long sum;
+	min = 1000000;
+	max = -1000000;
+    sum = 0;
+    int m;
+
+
+    for (int i = 0; i < n; i++)
+    {
+		std::cin >> m;
+
+        sum += m;
+        if (min > m)   min = m;
+
+        if (max < m)   max = m;
+
+		
+    }
+
+    std::cout << min << " " << max << " " << sum << std::endl;
+
+	return 0;
+}
+
 

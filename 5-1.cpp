e37f8574892307bd938faba328dbca67bf982a2b
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-16 22:55:07 +0900

    Update 5-1.cpp

--- 5-1.cpp
+++ 5-1.cpp
@@ -1,1 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include <climits>
+#include <cmath>
+
+int h, w;
+
+
+int main(){
+    std::cin >> h >> w;
+    while (h || w)
+    {
+        
+        for (int i = 0; i < h; i++){
+            for (int j = 0; j < w; j++)
+            {
+         
+                std::cout << "#";
+
+          
+            }
+            
+            std::cout << std::endl;
+        }
+        std::cout << std::endl;
+         std::cin >> h >> w;
+    }
+    
+
+    
+
+	return 0;
+}
+
 

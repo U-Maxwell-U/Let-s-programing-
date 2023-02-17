51ce2259d089f5790690824599e9b9c540b6bcd6
U-Maxwell-U <124647524+U-Maxwell-U@users.noreply.github.com>
2023-02-17 17:55:49 +0900

    Create 6-2.cpp

---
+++ 6-2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<stdio.h>
+#include <vector>
+
+
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    int r[n];
+    for(int i = 0; i < n; i++){
+        cin >> r[i];
+    }
+    for(int i = n - 1; i >= 0; i--){
+        cout << (i != n - 1 ? " ":"") << r[i];
+    }
+
+  cout << endl;
+  return 0;
+}
